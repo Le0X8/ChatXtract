8db62bc868fa5c40bc118d80c01866871010d406
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-10-02 14:59:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -17,22 +17,35 @@ client.on('qr', qr => {
 client.on('ready', () => {
     console.log('Authenticated successfully.');
     prompt('Press [ENTER] when sync has ended. ');
+
     var mediacount = 0;
-    fs.writeFile('./loot/' + id + '/clientinfo.json', JSON.stringify(client.info, null, 4), 'utf-8', (e) => {if (e != null) {console.error('An error occurred: ' + e);} else {console.log('Written client info successfully.');};});
+
+    fs.writeFile(
+        `./loot/${id}/clientinfo.json`,
+        JSON.stringify(client.info, null, 4),
+        'utf-8',
+        (e) => {
+            if (e != null) console.error('An error occurred: ' + e);
+            else console.log('Written client info successfully.');
+        }
+    );
+
     client.getContacts().then(val => fs.writeFile(`./loot/${id}/contacts.json`, JSON.stringify(val, null, 4), 'utf-8', (e) => {if (e != null) {console.error('An error occurred: ' + e);} else {console.log('Written contacts successfully.');};}));
+
     client.getChats().then(val => {
         fs.writeFile('./loot/' + id + '/chats.json', JSON.stringify(val, null, 4), 'utf-8', (e) => {if (e != null) {console.error('An error occurred: ' + e);} else {console.log('Written chat history successfully.');};});
         val.forEach(chat => {
             chat.fetchMessages({limit: 100000}).then(mval => {
                 fs.writeFile(`./loot/${id}/chats/${chat.id._serialized}.json`, JSON.stringify(mval, null, 4), 'utf-8', (e) => {if (e != null) {console.error('An error occurred: ' + e);} else {console.log('Written chat list successfully.');};});
-                mval.forEach(message => {if (message.hasMedia) {
-                    if (!fs.existsSync('./loot/' + id + '/chats/' + chat.id._serialized)){fs.mkdirSync('./loot/' + id + '/chats/' + chat.id._serialized);};
-                    message.downloadMedia().then(file => {
-                        if (file) {
-                            fs.writeFile('./loot/' + id + '/chats/' + chat.id._serialized + '/' + message.id.id + '.' + file.mimetype.replaceAll('/', 'Slash'), Buffer.from(file.data, 'base64'), 'utf-8', (e) => {if (e != null) {console.error('An error occurred: ' + e);} else {mediacount++; console.log('Downloaded media successfully. (' + mediacount + ')');};});
-                        };
-                    })
-                };
+                mval.forEach(message => {
+                    if (message.hasMedia) {
+                        if (!fs.existsSync('./loot/' + id + '/chats/' + chat.id._serialized)){fs.mkdirSync('./loot/' + id + '/chats/' + chat.id._serialized);};
+                        message.downloadMedia().then(file => {
+                            if (file) {
+                                fs.writeFile('./loot/' + id + '/chats/' + chat.id._serialized + '/' + message.id.id + '.' + file.mimetype.replaceAll('/', 'Slash'), Buffer.from(file.data, 'base64'), 'utf-8', (e) => {if (e != null) {console.error('An error occurred: ' + e);} else {mediacount++; console.log('Downloaded media successfully. (' + mediacount + ')');};});
+                            }
+                        })
+                    }
                 });
             });
         });
